fdfe21589acb85c6f1e674db48abb25b1d108ac1
Akanksha Rani <akankshar8800@gmail.com>
2023-03-31 04:18:54 +0530

    resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+import pdf from "../../Assets/../Assets/Akanksha_Rani_Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/AkankshaAI/portfolio/master/src/Assets/Akanksha_Rani_Resume.pdf";
+  "https://github.com/AkankshaAI/Akanksha-Portfolio/blob/master/src/Assets/Akanksha%20Rani%20Resume.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
